bee43e528701eb5253ff27bffb3210dbec7270ed
石川幹晴 <kansei2@kansei2.local>
2022-01-05 22:49:58 +0900

    test

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
+import sekitoba_data_manage as dm
+
 from argparse import ArgumentParser
 
-from data_analyze import data_create
-from machine_learn import learn
+#from data_analyze import data_create
+#from machine_learn import learn
 from simulation import simu_test
 
 def main():
-    parser = ArgumentParser()
-    parser.add_argument( "-g", type=bool, default = False, help = "optional" )
-    parser.add_argument( "-u", type=bool, default = False, help = "optional" )
+    #parser = ArgumentParser()
+    #parser.add_argument( "-g", type=bool, default = False, help = "optional" )
+    #parser.add_argument( "-u", type=bool, default = False, help = "optional" )
 
-    g_check = parser.parse_args().g
-    u_check = parser.parse_args().u
-    data = data_create.main( update = u_check )
-    model = learn.main( data )
-    simu_test.main( data, model )
+    #g_check = parser.parse_args().g
+    #u_check = parser.parse_args().u
+    #data = data_create.main( update = u_check )
+    #model = learn.main( data )
+    model = dm.pickle_load( "rank_model.pickle" )
+    simu_data = dm.pickle_load( "rank_simu_data.pickle" )
+    
+    simu_test.main( model, data )
     
 main()
--- simulation/simu_test.py
+++ simulation/simu_test.py
@@ -1,62 +1,90 @@
-import numpy as np
+import math
+import torch
 import random
+import numpy as np
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 import sekitoba_library as lib
 import sekitoba_data_manage as dm
 
-N = 1000
+from simulation import bet_select
 
-def probability( p_data ):
-    number = -1
-    count = 0
-    r_check = random.random()
+def softmax( data ):
+    result = []
+    sum_data = 0
+    value_max = max( data )
+
+    for i in range( 0, len( data ) ):
+        sum_data += math.exp( data[i] - value_max )
+
+    for i in range( 0, len( data ) ):
+        result.append( math.exp( data[i] - value_max ) / sum_data )
+
+    return result
+
+def main( model, data ):
+    recovery_rate = 0
+    test = {}
+    test_result = { "count": 0, "money": 0, "win": 0 }
+    N = 20
+    n = 5
+    money = 50000
+    ave_score = 0
+    win_score = 0
+    lose_score = 0
     
-    for i in range( 0, len( p_data ) ):
-        count += p_data[i]
+    for race_id in tqdm( data.keys() ):
+        horce_list = []
+        score_list = []
         
-        if r_check <= count:
-            number = i
-            break
+        for horce_id in data[race_id].keys():
+            p_data = model.predict( np.array( [ data[race_id][horce_id]["data"] ] ) )
+            ex_value = {}
+            score = p_data[0] * -1
+            ex_value["score"] = score
+            ex_value["rank"] = data[race_id][horce_id]["answer"]["rank"]
+            ex_value["odds"] = data[race_id][horce_id]["answer"]["odds"]
+            score_list.append( score )
+            horce_list.append( ex_value )
 
-    if number == -1:
-        number = len( p_data )
+        score_list = softmax( score_list )
 
-    return number    
-
-def main( data, model ):
-    count = 0
-    win = 0
-    money = 0
-    race_num = len( data["test_count"] )
-
-    for i in tqdm( range( 0, race_num ) ):
-        current_count = data["test_count"][i]
-        use_data = data["test_teacher"][count:count+current_count]
-        use_answer = data["test_answer"][count:count+current_count]
-        predict_data = model.forward( np.array( use_data, np.float32 ) ).data
-            
-        answer = sorted( use_answer, key=lambda x:x["rank"] )
-        predict_answer = []
-
-        for p_data in predict_data:
-            number = 0
-            
-            for n in range( 0, N ):
-                number += probability( p_data )
-
-            number /= N
-            predict_answer.append( { "diff": number, "count": len( predict_answer ) + 1 } )
-
-        predict_answer = sorted( predict_answer, key=lambda x:x["diff"] )
+        for i in range( 0, len( score_list ) ):
+            horce_list[i]["score"] = score_list[i]
         
-        if predict_answer[0]["count"] == answer[0]["count"]:
-            money += answer[0]["odds"]
-            win += 1
-
-        count += current_count
-
-    print( "勝率{}%\n回収率{}%".format( win / race_num * 100, money / race_num * 100 ) )
-                
+        sort_result = sorted( horce_list, key=lambda x:x["score"], reverse = True )
+        max_rate = 0
         
-    
+        rate_list = []
+        odds_list = []
+
+        for i in range( 0, n ):
+            rate_list.append( horce_list[i]["score"] )
+            odds_list.append( horce_list[i]["odds"] )
+
+        bet_list = bet_select.main( rate_list, odds_list, N )
+
+        for i in range( 0, n ):
+            bet_horce = sort_result[i]
+            test_result["count"] += bet_list[i]
+        
+            if bet_horce["rank"] == 1:
+                #recovery_rate += bet_horce["odds"] * bet_list[i]
+                #test_result["win"] += 1
+                test_result["money"] += bet_horce["odds"] * bet_list[i]
+                #lib.log.write( "odds:" + str( bet_horce["odds"] ) + " score:" + str( max( score_list ) ) )
+
+    recovery_rate = test_result["money"] / test_result["count"]
+    recovery_rate *= 100
+
+    print( "回収率{}%".format( recovery_rate ) )
+    #print( "勝率{}%".format( win_rate ) )
+    print( "賭けた回数{}回".format( test_result["count"] ) )
+    #lib.log.write( "回収率{}%".format( recovery_rate ) )
+    #lib.log.write( "勝率{}%".format( win_rate ) )
+    #lib.log.write( "賭けた回数{}回".format( test_result["count"] ) )
+
+
+
+
