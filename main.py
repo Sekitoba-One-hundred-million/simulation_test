61b1f9ec6dcc31347fd7e40150c0568c113d27b0
kansei <kansei@kansei.local>
2023-06-10 00:47:26 +0900

    add optuna

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
-import sekitoba_data_manage as dm
-
-from argparse import ArgumentParser
-
-#from data_analyze import data_create
-#from machine_learn import learn
-from simulation import simu_test
-
 def main():
-    #parser = ArgumentParser()
-    #parser.add_argument( "-g", type=bool, default = False, help = "optional" )
-    #parser.add_argument( "-u", type=bool, default = False, help = "optional" )
+    import sekitoba_data_manage as dm
+    from simulation import buy_simulation
+    from simulation import baren_simulation
+    from simulation import test
+    
+    #simu_data = dm.pickle_load( "rank_simu_data.pickle" )
+    #model = dm.pickle_load( "rank_model.pickle" )
+    buy_simulation.main()
+    #baren_simulation.main()
+    #test.main()
 
-    #g_check = parser.parse_args().g
-    #u_check = parser.parse_args().u
-    #data = data_create.main( update = u_check )
-    #model = learn.main( data )
-    model = dm.pickle_load( "rank_model.pickle" )
-    simu_data = dm.pickle_load( "rank_simu_data.pickle" )
-    
-    simu_test.main( model, data )
-    
-main()
+if __name__ == "__main__":
+    main()
